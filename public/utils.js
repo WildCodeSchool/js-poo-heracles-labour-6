2d2e6788b8923398365ba0d8b1c1c3b7daecfc7f
KevinLavigne <lavigne.kevin02@gmail.com>
2023-01-05 16:47:09 +0100

    correct images path on css. correct class Monster property by deleting sword and shield. correct utils.js accessory part for match with the step give on readme.md file.

--- public/utils.js
+++ public/utils.js
@@ -4,33 +4,33 @@
  * @param {string} root (# dans le HTML)
  */
 class TemplateRoot {
-  constructor(root) {
-    this.rootElement = document.getElementById(root);
-  }
+	constructor(root) {
+		this.rootElement = document.getElementById(root);
+	}
 
-  /**
-   * Inject the template on the corresponding DIV
-   * @param {String} template
-   */
-  render(template) {
-    const fragment = document.createElement('div');
-    fragment.innerHTML = template;
-    this.rootElement.appendChild(fragment);
-  }
+	/**
+	 * Inject the template on the corresponding DIV
+	 * @param {String} template
+	 */
+	render(template) {
+		const fragment = document.createElement('div');
+		fragment.innerHTML = template;
+		this.rootElement.appendChild(fragment);
+	}
 }
 
 class FightersTemplate extends TemplateRoot {
-  constructor(root) {
-    super(root);
-  }
+	constructor(root) {
+		super(root);
+	}
 
-  /**
-   * Creating the HTML template for the battle section
-   * @param {Object} hero
-   * @param {Object} enemy
-   */
-  createTemplate(hero, enemy) {
-    const fighterTemplate = `<div class="fighters">
+	/**
+	 * Creating the HTML template for the battle section
+	 * @param {Object} hero
+	 * @param {Object} enemy
+	 */
+	createTemplate(hero, enemy) {
+		const fighterTemplate = `<div class="fighters">
       <a href="#hero">
         <figure class="heracles">
           <img src="${hero.image}" alt="${hero.name}" />
@@ -44,8 +44,8 @@ class FightersTemplate extends TemplateRoot {
       </figure>
     </div>`;
 
-    this.render(fighterTemplate)
-  }
+		this.render(fighterTemplate);
+	}
 }
 
 /**
@@ -53,16 +53,16 @@ class FightersTemplate extends TemplateRoot {
  * @param {string} root
  */
 class InfoTemplate extends TemplateRoot {
-  constructor(root) {
-    super(root)
-  }
+	constructor(root) {
+		super(root);
+	}
 
-  /**
-   * Creating the html for the hero information
-   * @param {Object} hero
-   */
-  createHeroInfoTemplate(hero) {
-    const heroInfoTemplate = `<div class="hero" id="hero">
+	/**
+	 * Creating the html for the hero information
+	 * @param {Object} hero
+	 */
+	createHeroInfoTemplate(hero) {
+		const heroInfoTemplate = `<div class="hero" id="hero">
           <a href="#" class="close" onclick="closeModal()">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
@@ -70,13 +70,17 @@ class InfoTemplate extends TemplateRoot {
           </a>
           <div class="slots equipment" id="slot">
           <div data-slot="Main weapon" class="slot">
-            ${hero.weapon ? this.getImage(hero.weapon.image, "weapon") : ""}
+            ${hero.weapon ? this.getImage(hero.weapon.image, 'weapon') : ''}
           </div>
           <div data-slot="Shield" class="slot">
-            ${hero.shield ? this.getImage(hero.shield.image, "shield") : ""}
+            ${hero.shield ? this.getImage(hero.shield.image, 'shield') : ''}
           </div>
           <div data-slot="Secondary weapon" class="slot">
-            ${hero.accessorie ? this.getImage(hero.accessorie.image, "accessories") : ""}
+            ${
+							hero.accessory
+								? this.getImage(hero.accessory.image, 'accessory')
+								: ''
+						}
           </div>
           <div data-slot="Head" class="slot"></div>
           <div data-slot="Secondary Shield" class="slot">
@@ -85,16 +89,16 @@ class InfoTemplate extends TemplateRoot {
             ${hero.experience}
           </div>
           <div data-slot="Attack" class="slot statistic">
-              ${hero.getDamage ? hero.getDamage() : ""}
+              ${hero.getDamage ? hero.getDamage() : ''}
           </div>
           <div data-slot="Defense" class="slot statistic">
-              ${hero.getDefense ? hero.getDefense() : ""}
+              ${hero.getDefense ? hero.getDefense() : ''}
           </div>
           <div data-slot="Life" class="slot statistic">
             ${hero.life}
           </div>
           <div data-slot="Range" class="slot statistic">
-            ${hero.getRange ? hero.getRange() : ""}
+            ${hero.getRange ? hero.getRange() : ''}
           </div>
       </div >
       <div class="character">
@@ -102,23 +106,23 @@ class InfoTemplate extends TemplateRoot {
         <div class="avatar">
           <img src="${hero.image}" alt="heracles">
         </div>
-        <p class="level">${hero.getLevel ? hero.getLevel() : ""}</p>
+        <p class="level">${hero.getLevel ? hero.getLevel() : ''}</p>
       </div>
     </div > `;
 
-    this.render(heroInfoTemplate)
-  }
+		this.render(heroInfoTemplate);
+	}
 
-  getImage(url, alt) {
-    return `<img src=${url} alt=${alt} />`
-  }
+	getImage(url, alt) {
+		return `<img src=${url} alt=${alt} />`;
+	}
 
-  /**
-   * Creating the Html for the monsters information
-   * @param {Object} ennemys
-   */
-  createEnnemyInfoTemplate(ennemys) {
-    const ennemyInfoTemplate = `<div class="hero" id="hero" >
+	/**
+	 * Creating the Html for the monsters information
+	 * @param {Object} ennemys
+	 */
+	createEnnemyInfoTemplate(ennemys) {
+		const ennemyInfoTemplate = `<div class="hero" id="hero" >
           <a href="#" class="close" onclick="closeModal()">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
@@ -129,39 +133,42 @@ class InfoTemplate extends TemplateRoot {
           </div >
         </div > `;
 
-    this.render(ennemyInfoTemplate)
-  }
+		this.render(ennemyInfoTemplate);
+	}
 
-
-  /**
-   * Creating the monsters info parts
-   * @param {Object} ennemys
-   * @returns Array of String
-   */
-  getAllEnnemyDescription(ennemys) {
-    return ennemys.map(ennemy => {
-      return `<div class="slots ennemy" id="slot">
-      <div class="slot" data-slot=" Level ${ennemy.getLevel ? ennemy.getLevel() : ""}" >
-        <h2 class="name">${ennemy.name} ${ennemy.life > 0 ? "" : "DEAD"}</h2>
+	/**
+	 * Creating the monsters info parts
+	 * @param {Object} ennemys
+	 * @returns Array of String
+	 */
+	getAllEnnemyDescription(ennemys) {
+		return ennemys.map((ennemy) => {
+			return `<div class="slots ennemy" id="slot">
+      <div class="slot" data-slot=" Level ${
+				ennemy.getLevel ? ennemy.getLevel() : ''
+			}" >
+        <h2 class="name">${ennemy.name} ${ennemy.life > 0 ? '' : 'DEAD'}</h2>
       </div >
       <div class="slot">
-          <img src="${ennemy.image}" alt="heracles" class="${ennemy.life > 0 ? "" : "dead"}">
+          <img src="${ennemy.image}" alt="heracles" class="${
+				ennemy.life > 0 ? '' : 'dead'
+			}">
       </div>
       <div data-slot="Attack" class="slot statistic">
-          ${ennemy.getDamage ? ennemy.getDamage() : ""}
+          ${ennemy.getDamage ? ennemy.getDamage() : ''}
       </div>
       <div data-slot="Defense" class="slot statistic">
-          ${ennemy.getDefense ? ennemy.getDefense() : ""}
+          ${ennemy.getDefense ? ennemy.getDefense() : ''}
       </div>
       <div data-slot="Life" class="slot statistic">
         ${ennemy.life}
       </div>
       <div data-slot="Range" class="slot statistic">
-        ${ennemy.getRange ? ennemy.getRange() : ""}
+        ${ennemy.getRange ? ennemy.getRange() : ''}
       </div>
-    </div> `
-    })
-  }
+    </div> `;
+		});
+	}
 }
 
 /**
@@ -170,161 +177,182 @@ class InfoTemplate extends TemplateRoot {
  * @param {string} root
  */
 class ArenaTemplate extends TemplateRoot {
-  constructor(root) {
-    super(root);
-  }
+	constructor(root) {
+		super(root);
+	}
 
-  /**
-   * Check for each case if there is a Monster or Tiles or Hero
-   * @param {Object} arena
-   * @param {Number} i
-   * @param {Number} j
-   * @returns String
-   */
-  checkCards(arena, i, j) {
-    let img = '';
+	/**
+	 * Check for each case if there is a Monster or Tiles or Hero
+	 * @param {Object} arena
+	 * @param {Number} i
+	 * @param {Number} j
+	 * @returns String
+	 */
+	checkCards(arena, i, j) {
+		let img = '';
 
-    arena.tiles.forEach((tile, index) => {
-      if (tile.x === i && tile.y === j) {
-        img = this.makeTile(tile)
-      }
-    })
+		arena.tiles.forEach((tile, index) => {
+			if (tile.x === i && tile.y === j) {
+				img = this.makeTile(tile);
+			}
+		});
 
-    arena.monsters.forEach((monster, index) => {
-      if (monster.x === i && monster.y === j) {
-        img = this.makeMonsterImage(arena, index)
-      }
-    })
+		arena.monsters.forEach((monster, index) => {
+			if (monster.x === i && monster.y === j) {
+				img = this.makeMonsterImage(arena, index);
+			}
+		});
 
-    if (arena.hero.x === i && arena.hero.y === j) {
-      img = this.makeHeroImage(arena)
-    }
-    return img;
-  }
+		if (arena.hero.x === i && arena.hero.y === j) {
+			img = this.makeHeroImage(arena);
+		}
+		return img;
+	}
 
-  /**
-   * Creating the HTML for a tile
-   * @param {Object} tile
-   * @returns String
-   */
-  makeTile(tile) {
-    return `<img class="tile" src="${tile.image}" alt="" />`
-  }
+	/**
+	 * Creating the HTML for a tile
+	 * @param {Object} tile
+	 * @returns String
+	 */
+	makeTile(tile) {
+		return `<img class="tile" src="${tile.image}" alt="" />`;
+	}
 
-  /**
-   * Creating the HTML for the hero
-   * @param {Object} arena
-   * @returns
-   */
-  makeHeroImage(arena) {
-    return `<img title="${arena.hero.name}, portée de ${arena.hero.getRange ? arena.hero.getRange() : ""}" alt="${arena.hero.name}" src="${arena.hero.image}" > `
-  }
+	/**
+	 * Creating the HTML for the hero
+	 * @param {Object} arena
+	 * @returns
+	 */
+	makeHeroImage(arena) {
+		return `<img title="${arena.hero.name}, portée de ${
+			arena.hero.getRange ? arena.hero.getRange() : ''
+		}" alt="${arena.hero.name}" src="${arena.hero.image}" > `;
+	}
 
-  /**
-   * Creating the HTML for the Monster
-   * @param {Object} arena
-   * @param {Number} index
-   * @returns String
-   */
-  makeMonsterImage(arena, index) {
-    if (arena.monsters[index].isAlive()) {
-      return `<img 
+	/**
+	 * Creating the HTML for the Monster
+	 * @param {Object} arena
+	 * @param {Number} index
+	 * @returns String
+	 */
+	makeMonsterImage(arena, index) {
+		if (arena.monsters[index].isAlive()) {
+			return `<img 
         id="monster_${index}" 
         src="${arena.monsters[index].image}" 
-        title="Distance to ${arena.hero.name} ${arena.getDistance ? arena.getDistance(arena.monsters[index], arena.hero) : ""}" 
-        class="monster ${arena.isTouchable ? (arena.isTouchable(arena.hero, arena.monsters[index]) ? 'touchable' : 'untouchable') : ""}" 
-      />`
-    }
-    return "";
-  }
+        title="Distance to ${arena.hero.name} ${
+				arena.getDistance
+					? arena.getDistance(arena.monsters[index], arena.hero)
+					: ''
+			}" 
+        class="monster ${
+					arena.isTouchable
+						? arena.isTouchable(arena.hero, arena.monsters[index])
+							? 'touchable'
+							: 'untouchable'
+						: ''
+				}" 
+      />`;
+		}
+		return '';
+	}
 
-  /**
-   * Creating the full HTML template for the arena
-   * @param {Object} arena
-   */
-  createArena(arena) {
-    const arenaDiv = [];
-    for (let i = 0; i < arena.size; i++) {
-      for (let j = 0; j < arena.size; j++) {
-        arenaDiv.push(`<div id="pos${i}${j}" >
+	/**
+	 * Creating the full HTML template for the arena
+	 * @param {Object} arena
+	 */
+	createArena(arena) {
+		const arenaDiv = [];
+		for (let i = 0; i < arena.size; i++) {
+			for (let j = 0; j < arena.size; j++) {
+				arenaDiv.push(`<div id="pos${i}${j}" >
       ${this.checkCards(arena, i, j)}
         </div>`);
-      }
-    }
-    const arenaTemplate = `${arena.digArena ? `<button class="btn btn-primary btn-shovel" id="digButton"><img src="./images/shovel.svg"><span>Dig</span></button>` : ''}
+			}
+		}
+		const arenaTemplate = `${
+			arena.digArena
+				? `<button class="btn btn-primary btn-shovel" id="digButton"><img src="./images/shovel.svg"><span>Dig</span></button>`
+				: ''
+		}
       <div class="map" style="--tiles-number: ${arena.size}" >
       <img class="boussole" src="./images/boussole.svg" alt="boussole" />
         ${arenaDiv.join('')}
       </div>`;
 
-    document.getElementById("arena").innerHTML = arenaTemplate;
-    if (arena.digArena && arena.hero.accessorie.role === "diggeable") {
-      this.setDigEvent(arena)
-    }
-  }
+		document.getElementById('arena').innerHTML = arenaTemplate;
+		if (arena.digArena && arena.hero.accessorie.role === 'diggeable') {
+			this.setDigEvent(arena);
+		}
+	}
 
-  /**
-   * Initialize the event Listener on the Dig button
-   * @param {*} arena 
-   */
-  setDigEvent(arena) {
-    document.getElementById('digButton').addEventListener('click', () => {
-      const isDigged = arena.digArena();
-      if (isDigged) this.createArena(arena);
-    })
-  }
+	/**
+	 * Initialize the event Listener on the Dig button
+	 * @param {*} arena
+	 */
+	setDigEvent(arena) {
+		document.getElementById('digButton').addEventListener('click', () => {
+			const isDigged = arena.digArena();
+			if (isDigged) this.createArena(arena);
+		});
+	}
 
-  /**
-   * Initialize the movement on the arena
-   * @param {Object} arena
-   */
-  setMoveEvent(arena) {
-    document.addEventListener('keydown', (event) => {
-      const keyName = event.key;
-      const directions = { ArrowUp: 'N', ArrowDown: 'S', ArrowRight: 'E', ArrowLeft: 'W' };
+	/**
+	 * Initialize the movement on the arena
+	 * @param {Object} arena
+	 */
+	setMoveEvent(arena) {
+		document.addEventListener('keydown', (event) => {
+			const keyName = event.key;
+			const directions = {
+				ArrowUp: 'N',
+				ArrowDown: 'S',
+				ArrowRight: 'E',
+				ArrowLeft: 'W',
+			};
 
-      if (keyName in directions) {
-        event.preventDefault();
+			if (keyName in directions) {
+				event.preventDefault();
 
-        arena.globalMove(directions[keyName], arena.hero);
-        this.createArena(arena);
-        this.setMonsterClick(arena);
-      }
-    });
-  }
+				arena.globalMove(directions[keyName], arena.hero);
+				this.createArena(arena);
+				this.setMonsterClick(arena);
+			}
+		});
+	}
 
-  /**
-   * Initialize the click on monster each time the arena is rebuild
-   * @param {Object} arena
-   */
-  setMonsterClick(arena) {
-    document.querySelectorAll(".monster").forEach(monster => {
-      monster.addEventListener('click', (e) => {
-        const index = monster.id.split("_")[1];
-        const dead = arena.battle(index)
-        if (dead) {
-          this.createArena(arena);
-        }
-      })
-    })
-  }
+	/**
+	 * Initialize the click on monster each time the arena is rebuild
+	 * @param {Object} arena
+	 */
+	setMonsterClick(arena) {
+		document.querySelectorAll('.monster').forEach((monster) => {
+			monster.addEventListener('click', (e) => {
+				const index = monster.id.split('_')[1];
+				const dead = arena.battle(index);
+				if (dead) {
+					this.createArena(arena);
+				}
+			});
+		});
+	}
 }
 
 /** Do not touch => allow the opening / closing of the hero information section */
 let openingModal = true;
 const openModal = (type) => {
-  if (openingModal) {
-    const info = new InfoTemplate('info');
-    if (type === "hero") info.createHeroInfoTemplate(heracles);
-    if (type === "ennemy") info.createEnnemyInfoTemplate([Ceryneian]);
-    document.getElementById("info").style.display = "flex";
-    openingModal = false;
-  }
-}
+	if (openingModal) {
+		const info = new InfoTemplate('info');
+		if (type === 'hero') info.createHeroInfoTemplate(heracles);
+		if (type === 'ennemy') info.createEnnemyInfoTemplate([Ceryneian]);
+		document.getElementById('info').style.display = 'flex';
+		openingModal = false;
+	}
+};
 
 const closeModal = () => {
-  const heroInfo = document.getElementById("info");
-  heroInfo.style.display = "none";
-  heroInfo.innerHTML = "";
-  openingModal = true;
-}
+	const heroInfo = document.getElementById('info');
+	heroInfo.style.display = 'none';
+	heroInfo.innerHTML = '';
+	openingModal = true;
+};
